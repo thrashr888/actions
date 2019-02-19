73bc254de576b503178cf474230428c72ee5b6dd
Paul Thrasher <thrashr888@gmail.com>
2019-02-19 09:25:34 -0800

    Update entrypoint.sh

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 
 SSH_PATH="$HOME/.ssh"
 
+echo "==> copying keys"
+
 mkdir -p "$SSH_PATH"
 touch "$SSH_PATH/known_hosts"
 
@@ -15,10 +17,14 @@ chmod 600 "$SSH_PATH/known_hosts"
 chmod 600 "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key.pub"
 
+echo "==> ssh-agent"
 eval $(ssh-agent)
+
+echo "==> ssh-add \"$SSH_PATH/deploy_key\""
 ssh-add "$SSH_PATH/deploy_key"
 
+echo "==> ssh-keyscan -t rsa $HOST >> \"$SSH_PATH/known_hosts\""
 ssh-keyscan -t rsa $HOST >> "$SSH_PATH/known_hosts"
 
-echo "ssh -A -tt -o 'StrictHostKeyChecking=no' -p ${PORT:-22} $USER@$HOST \"$*\""
+echo "==> ssh -A -tt -o 'StrictHostKeyChecking=no' -p ${PORT:-22} $USER@$HOST \"$*\""
 ssh -A -tt -o 'StrictHostKeyChecking=no' -p ${PORT:-22} $USER@$HOST "$*"
