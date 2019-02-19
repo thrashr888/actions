bc1e649139066aa25331eccc3ceb2cce8997ba45
Paul Thrasher <thrashr888@gmail.com>
2019-02-19 00:20:39 -0800

    Update entrypoint.sh

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -20,6 +20,4 @@ ssh-add "$SSH_PATH/deploy_key"
 
 ssh-keyscan -t rsa $HOST >> "$SSH_PATH/known_hosts"
 
-# ssh -A -tt -o 'StrictHostKeyChecking=no' $USER@$HOST "$*"
-echo "ssh -A -tt -o 'StrictHostKeyChecking=no' $USER@$HOST $*"
-sh -c "ssh -A -tt -o 'StrictHostKeyChecking=no' $USER@$HOST $*"
+ssh -A -tt -o 'StrictHostKeyChecking=no' -p ${PORT:-22} $USER@$HOST "$*"
